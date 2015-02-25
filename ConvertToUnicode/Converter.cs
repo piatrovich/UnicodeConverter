dbb7cdb02faa065380de9795a7145d112c039887
Dzmitry Piatrovich <piatrovich@live.ru>
2015-02-25 23:30:34 +0300

    first release version

---
+++ ConvertToUnicode/Converter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertToUnicode
+{
+    class Converter
+    {
+
+        public static string ToUnicode(string input)
+        {
+            Encoding encoding = new UnicodeEncoding();
+            string unicode = string.Empty;
+            byte[] bytes = encoding.GetBytes(input);
+            for (int i = 0; i < bytes.Length; i += 2)
+            {
+                unicode += string.Format("\\u{0:x2}", bytes[i + 1]);
+                unicode += string.Format("{0:x2}", bytes[i]);
+            }
+            return unicode;
+        }
+
+    }
+}
---
+++ ConvertToUnicode/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ConvertToUnicode
+{
+    partial class converter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input = new System.Windows.Forms.TextBox();
+            this.output = new System.Windows.Forms.TextBox();
+            this.convert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(12, 12);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(814, 20);
+            this.input.TabIndex = 0;
+            this.input.TextChanged += new System.EventHandler(this.input_TextChanged);
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(12, 49);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(814, 20);
+            this.output.TabIndex = 1;
+            this.output.TextChanged += new System.EventHandler(this.output_TextChanged);
+            // 
+            // convert
+            // 
+            this.convert.Location = new System.Drawing.Point(844, 12);
+            this.convert.Name = "convert";
+            this.convert.Size = new System.Drawing.Size(75, 57);
+            this.convert.TabIndex = 2;
+            this.convert.Text = "Convert";
+            this.convert.UseVisualStyleBackColor = true;
+            this.convert.Click += new System.EventHandler(this.convert_Click);
+            // 
+            // converter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(936, 87);
+            this.Controls.Add(this.convert);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.input);
+            this.Name = "converter";
+            this.Text = "Unicode converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.Button convert;
+    }
+}
+
